cf56fe6ce576d40f4c385d0e764a55c82a2a36a9
Vadim Kravcov <v.sem4ik@mail.ru>
2023-10-21 16:30:59 +0300

    add hw try 3

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from catalog.models import Product
+from catalog.models import Product, Version
 
 class ProductForm(forms.ModelForm):
 
@@ -27,3 +27,18 @@ class ProductForm(forms.ModelForm):
                 raise forms.ValidationError('Описание содержит запрещенные слова')
 
         return cleaned_data
+
+
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class VersionForm(StyleFormMixin, forms.ModelForm):
+
+    class Meta:
+        model = Version
+        fields = '__all__'
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,4 @@
+from django.forms import inlineformset_factory
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy, reverse
 
@@ -5,12 +6,14 @@ from catalog.models import Product
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.shortcuts import redirect
 
-from catalog.forms import ProductForm
+from catalog.forms import ProductForm, VersionForm
+from catalog.models import Version
 
 
 class ProductListView(ListView):
     model = Product
     # template_name = 'catalog/testmain.html'
+
 
 
 # def index(request):
@@ -33,6 +36,11 @@ def contacts(request):
 class MerchandiseListView(ListView):
     model = Product
     template_name = 'catalog/merchandise.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['version'] = Version.objects.all()
+        return context
 
 
 # def merchandise(request):
@@ -81,6 +89,26 @@ class ProductUpdateView(UpdateView):
     model = Product
     form_class = ProductForm
     success_url = reverse_lazy('catalog:merchandise')
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        VersionFormset = inlineformset_factory(Product, Version, form=VersionForm, extra=1)
+        if self.request.method == 'POST':
+            formset = VersionFormset(self.request.POST, instance=self.object)
+        else:
+            formset = VersionFormset(instance=self.object)
+        context_data['formset'] = formset
+        return context_data
+
+    def form_valid(self, form):
+        context_data = self.get_context_data()
+        formset = context_data['formset']
+        self.object = form.save()
+
+        if formset.is_valid():
+            formset.instance = self.object
+            formset.save()
+        return super().form_valid(form)
 
 
 
